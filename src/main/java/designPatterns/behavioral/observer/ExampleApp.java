e27b61ef53832db2c08a80487f480d2d0cdd5e56
mihaifratila <mfratila@griddynamics.com>
2023-03-05 21:01:41 +0200

    Add an example of the Observer Behavioral Design Pattern

--- src/main/java/designPatterns/behavioral/observer/ExampleApp.java
+++ src/main/java/designPatterns/behavioral/observer/ExampleApp.java
@@ -1,12 +1,22 @@
 package designPatterns.behavioral.observer;
 
 
+import designPatterns.behavioral.observer.editor.Editor;
+import designPatterns.behavioral.observer.listeners.EmailNotificationListener;
+import designPatterns.behavioral.observer.listeners.LogOpenListener;
 
 public class ExampleApp {
 
     public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
 
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
