0f545b3be182663a7eea401364d481747fdf0220
mihaifratila <mfratila@griddynamics.com>
2023-03-06 01:26:56 +0200

    Add an example of the Template Method Behavioral Design Pattern

--- src/main/java/designPatterns/behavioral/templateMethod/ExampleApp.java
+++ src/main/java/designPatterns/behavioral/templateMethod/ExampleApp.java
@@ -2,11 +2,39 @@ package designPatterns.behavioral.templateMethod;
 
 
 
+import designPatterns.behavioral.templateMethod.networks.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Demo class. Everything comes together here.
+ */
 public class ExampleApp {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name: ");
+        String userName = reader.readLine();
+        System.out.print("Input password: ");
+        String password = reader.readLine();
 
-    public static void main(String[] args) {
+        // Enter the message.
+        System.out.print("Input message: ");
+        String message = reader.readLine();
 
+        System.out.println("\nChoose social network for posting message.\n" +
+                "1 - Facebook\n" +
+                "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+
+        // Create proper network object and send the message.
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
     }
-
-
 }
