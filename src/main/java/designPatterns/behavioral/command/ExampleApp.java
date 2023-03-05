69a1bb431559f6bef2b53dc091c1784ae0252574
mihaifratila <mfratila@griddynamics.com>
2023-03-05 11:52:19 +0200

    Add an example of the Command Behavioral Design Pattern

--- src/main/java/designPatterns/behavioral/command/ExampleApp.java
+++ src/main/java/designPatterns/behavioral/command/ExampleApp.java
@@ -1,12 +1,12 @@
 package designPatterns.behavioral.command;
 
 
+import designPatterns.behavioral.command.editor.Editor;
 
 public class ExampleApp {
 
     public static void main(String[] args) {
-
+        Editor editor = new Editor();
+        editor.init();
     }
-
-
 }
