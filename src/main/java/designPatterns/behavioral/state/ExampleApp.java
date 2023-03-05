bbd713df40d944dd8b275135c3f95ed354ac209b
mihaifratila <mfratila@griddynamics.com>
2023-03-06 00:20:13 +0200

    Add an example of the State Behavioral Design Pattern

--- src/main/java/designPatterns/behavioral/state/ExampleApp.java
+++ src/main/java/designPatterns/behavioral/state/ExampleApp.java
@@ -1,12 +1,13 @@
 package designPatterns.behavioral.state;
 
-
+import designPatterns.behavioral.state.ui.Player;
+import designPatterns.behavioral.state.ui.UI;
 
 public class ExampleApp {
 
     public static void main(String[] args) {
-
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
     }
-
-
 }
---
+++ src/main/java/designPatterns/behavioral/state/states/State.java
@@ -0,0 +1,23 @@
+package designPatterns.behavioral.state.states;
+
+import designPatterns.behavioral.state.ui.Player;
+
+/**
+ * Common interface for all states.
+ */
+public abstract class State {
+    Player player;
+
+    /**
+     * Context passes itself through the state constructor. This may help a
+     * state to fetch some useful context data if needed.
+     */
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
